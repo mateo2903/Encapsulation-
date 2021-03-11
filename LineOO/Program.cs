fe5f8667425eedc4d9f67ab8dd2b7965f46ba541
Mateo Jadrušić <majadrus@vsite.hr>
2021-03-12 00:26:29 +0100

    Add OO implementation of line intersection

---
+++ LineOO/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LineOO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var line1 = new Line(1, 2);
+            var line2 = new Line(2, 2);
+
+            var intersection = line1.Intersection(line2);
+            if (intersection == null)
+                Console.WriteLine("Lines are paralel");
+            else
+            {
+                Point p = intersection.Value;
+                Console.WriteLine($"Intersection: ({p.X}, {p.Y})");
+            }
+            
+        }
+    }
+}
